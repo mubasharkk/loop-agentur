59768e9d9af81dcf8b97e703f1ae27e3e80e4385
Mubashar Khokhar <mubashar.khokhar@gastfreund.net>
2023-12-10 21:53:44 +0100

    TestCase for Master data console cmd added

---
+++ tests/Unit/ImportMasterdataCommandTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class ImportMasterdataCommandTest extends TestCase
+{
+    use DatabaseMigrations, DatabaseTransactions;
+
+    /**
+     * A basic unit test example.
+     */
+    public function test_metadata_artisan_cmd(): void
+    {
+        $this->artisan('import:master-data')->assertSuccessful();
+        $this->assertDatabaseCount('customers', 10000);
+        $this->assertDatabaseCount('products', 100);
+    }
+}
